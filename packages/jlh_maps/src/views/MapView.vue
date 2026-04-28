668c97c179f6f07946bc5780606bc23f0431ba8c
Meterius <Meterius@users.noreply.github.com>
2026-04-28 19:41:16 +0200

    connect maplibre with geo infra tileserver and add basic controls

--- packages/jlh_maps/src/views/MapView.vue
+++ packages/jlh_maps/src/views/MapView.vue
@@ -1,12 +1,30 @@
-<script setup lang="ts">
-import { MglMap } from '@indoorequal/vue-maplibre-gl'
-</script>
-
 <template>
   <div style="position: absolute; left: 0; right: 0; top: 0; bottom: 0">
-    <mgl-map map-style="https://demotiles.maplibre.org/style.json" />
+    <mgl-map :map-style="mapStyleUrl" />
   </div>
 </template>
+
+<script setup lang="ts">
+import {MglMap, useMap, MglGeolocateControl, MglNavigationControl} from '@indoorequal/vue-maplibre-gl'
+import {watch} from "vue";
+import {GeolocateControl, GlobeControl, NavigationControl} from "maplibre-gl";
+
+const mapInstance = useMap();
+
+const mapStyleUrl = import.meta.env.VITE_OTM_TILESERVER_TILEJSON_URL;
+console.log(`Map style URL: ${mapStyleUrl}`);
+
+const unwatch = watch(mapInstance, (val) => {
+  if (val.map !== undefined) {
+    unwatch();
+    const map = val.map;
+
+    map.addControl(new GlobeControl());
+    map.addControl(new NavigationControl());
+    map.addControl(new GeolocateControl({}));
+  }
+});
+</script>
 
 <style lang="css">
 @import 'maplibre-gl/dist/maplibre-gl.css';

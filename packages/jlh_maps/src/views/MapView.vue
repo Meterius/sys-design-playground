f86ecfbb187ca969699dec50e762aad5dbb19b5d
Meterius <Meterius@users.noreply.github.com>
2026-04-28 18:49:48 +0200

    setup basic maplibre gl vue render

---
+++ packages/jlh_maps/src/views/MapView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { MglMap } from '@indoorequal/vue-maplibre-gl'
+</script>
+
+<template>
+  <div style="position: absolute; left: 0; right: 0; top: 0; bottom: 0">
+    <mgl-map map-style="https://demotiles.maplibre.org/style.json" />
+  </div>
+</template>
+
+<style lang="css">
+@import 'maplibre-gl/dist/maplibre-gl.css';
+</style>

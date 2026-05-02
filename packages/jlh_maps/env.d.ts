e7c76bbe55b0d4e4fed329f3a95525825a5c41e0
Meterius <Meterius@users.noreply.github.com>
2026-05-02 12:23:04 +0200

    add valhalla to geo infra

--- packages/jlh_maps/env.d.ts
+++ packages/jlh_maps/env.d.ts
@@ -1,14 +1,15 @@
 /// <reference types="vite/client" />
 
 interface ViteTypeOptions {
-    strictImportMetaEnv: unknown
+  strictImportMetaEnv: unknown
 }
 
 interface ImportMetaEnv {
-    readonly VITE_TILESERVER_OMT_URL: string
-    readonly VITE_API_URL: string
+  readonly VITE_TILESERVER_OMT_URL: string
+  readonly VITE_API_URL: string
+  readonly VITE_VALHALLA_URL: string
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv
 }
